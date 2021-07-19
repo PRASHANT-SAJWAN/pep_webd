68a558a0a4ddfc8dde7fa94596e884b3c6ded988
prashant_wan <pokerosajwan0@gmail.com>
2021-07-19 21:32:11 +0530

    carousel

--- josh/script.js
+++ josh/script.js
@@ -1,12 +1,32 @@
-let carouselFirstVisibleElemenet = 0;
-let carouselLastVisibleElemenet = 2;
-let carouselTotalElemenet = 6;
-let blogList = document.querySelector('.blog-posts .blog-list')
-let allBlogDiv = document.querySelectorAll('.blog-posts .blog-list .blogs');
+let headerlinks = document.querySelectorAll('.header .header-links .links');
 
-const carouselPrevSlide = () => {
-    
+for (let i = 0; i < headerlinks.length; ++i) {
+    headerlinks[i].addEventListener("click", (e) => {
+        let selectedLink = document.querySelector('.header .header-links .links.selected-link');
+        selectedLink.classList.remove('selected-link');
+        e.target.classList.add('selected-link');
+    });
 }
+
+
+let carouselIdx = 0;
+let blogList = document.querySelector('.blog-posts .blog-list')
+let allBlogBlockDiv = document.querySelectorAll('.blog-posts .blog-list .blog-block');
+let totalCarousel = allBlogBlockDiv.length;
 
 const carouselNextSlide = () => {
+    if (carouselIdx + 1 < totalCarousel) {
+        blogBlock
+    }
 }
+
+const carouselPrevSlide = () => {
+    if (carouselIdx - 1 >= 0) {
+        
+    }
+}
+
+let blogBlock = document.querySelector('.blog-posts .blog-list .blog-block.show');
+blogBlock.addEventListener("click", (e) => {
+    // console.log(e.target);
+})
